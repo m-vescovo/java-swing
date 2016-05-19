e8f9a8805fe699df14ebe0609c5ce101345529ef
tss <tss@debian-tss24>
2016-05-19 21:26:58 +0200

    bottone

--- src/Lezione10/App.java
+++ src/Lezione10/App.java
@@ -1,13 +1,28 @@
 
 package Lezione10;
 
+import java.awt.Container;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+
+
 public class App {
     
     public static void main (String[] args){
-        System.out.println("java swing example..");
-        System.out.println("finish");
-        System.out.println("terza");
-        System.out.println("stampa di prova");
+        
+       JFrame  f= new JFrame("esempio swing"); //creo la finestra
+       f.setBounds(100,100,300,200);//stabilisco le dimensioni della finestra
+       f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //per chiudere il programma quando chiudi la finestra
+ 
+       Container contentPane = f.getContentPane();
+       
+       JButton cmd = new JButton ("invia");
+       cmd.setBounds(10, 10, 50, 20);
+       contentPane.add(cmd);
+       
+       f.setVisible(true);//visualizzo la finestra
+               
     }
     
 }
