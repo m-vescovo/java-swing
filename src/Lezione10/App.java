4ad23c40f129e313b28bff75ad1e7ffc6d5256e2
tss <tss@debian-tss24>
2016-05-19 21:29:08 +0200

    bottone

--- src/Lezione10/App.java
+++ src/Lezione10/App.java
@@ -19,7 +19,7 @@ public class App {
        
        contentPane.setLayout(null);//blocca il layout di standard
        JButton cmd = new JButton ("invia");
-       cmd.setBounds(10, 10, 50, 20);
+       cmd.setBounds(50, 50, 100, 50);
        contentPane.add(cmd);
        
        f.setVisible(true);//visualizzo la finestra
