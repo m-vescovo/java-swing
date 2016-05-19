f8c0f3325eaf1296271f06b6613f4b1cd105f07e
tss <tss@debian-tss24>
2016-05-19 19:51:26 +0200

    seconda stampa

--- src/Lezione10/App.java
+++ src/Lezione10/App.java
@@ -14,6 +14,7 @@ public class App {
     public static void main (String[] args){
         System.out.println("java swing example..");
         System.out.println("finish");
+        System.out.println("terza");
     }
     
 }
